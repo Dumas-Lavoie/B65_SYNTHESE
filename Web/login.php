dad77b714ff5d9227b6a86d522d3ebf37aa76da0
Félix Dumas-Lavoie <goglu1@live.ca>
2019-12-01 15:32:44 -0500

    Login.php refactoring, login front-end

--- Web/login.php
+++ Web/login.php
@@ -1,48 +1,35 @@
 <?php
-	require_once("action/LoginAction.php");
 
-	$action = new LoginAction();
-	$action->execute();
+require_once("action/LoginAction.php");
+$action = new LoginAction();
+$action->execute();
 
-	require_once("partial/header.php");
+require_once("partial/header.php");
 ?>
 
-	<h1>Connexion</h1>
 
-	<div class="login-form-frame">
-		<form action="login.php" method="post">
-			<?php
-				if ($action->wrongLogin) {
-					?>
-					<div class="error-div"><strong>Erreur : </strong>Connexion erronée</div>
-					<?php
-				}
+<div id="login">
+	<form class="form-signin" action="login.php" method="POST">
+		<?php
+		if ($action->wrongLogin) {
 			?>
-
-			<div class="form-label">
-				<label for="username">Nom d'usager : </label>
+			<div class="alert alert-danger">
+				<strong>Erreur de connexion</strong>
 			</div>
-			<div class="form-input">
-				<input type="text" name="username" id="username" />
-			</div>
-			<div class="form-separator"></div>
-
-			<div class="form-label">
-				<label for="password">Mot de passe : </label>
-			</div>
-			<div class="form-input">
-				<input type="password" name="pwd" id="password" />
-			</div>
-			<div class="form-separator"></div>
-
-			<div class="form-label">
-				&nbsp;
-			</div>
-			<div class="form-input">
-				<button type="submit">Connexion</button>
-			</div>
-			<div class="form-separator"></div>
+		<?php
+		}
+		?>
+		<h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
+		<label for="inputUsername" class="sr-only">Username</label>
+		<input type="t" id="inputUsername" class="form-control" placeholder="Username" name="username" required="true" autofocus="true" value="<?php echo isset($_COOKIE["remember-me"]) ? $_COOKIE["remember-me"] : "" ?>">
+		<label for="inputPassword" class="sr-only">Password</label>
+		<input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required="true">
+		<div class="checkbox mb-3">
+			<label id="remember-me">
+				<input type="checkbox" value="remember-me" name="remember-me">Se souvenir</input>
+			</label>
+		</div>
+		<button class="btn btn-lg btn-primary" type="submit">Connexion</button>
+		<p class="mt-5 mb-3 text-muted">© 2017-2019</p>
 		</form>
-	</div>
-<?php
-	require_once("partial/footer.php");
+</div>
