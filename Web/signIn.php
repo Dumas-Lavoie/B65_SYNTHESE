eec632cfdcac8f8d6ac1b4a4e72462da7afb73a6
Dumas-Lavoie <goglu1@live.ca>
2019-12-11 15:09:57 -0500

    Correction html sign-in

--- Web/signIn.php
+++ Web/signIn.php
@@ -28,18 +28,18 @@
 
 			<div class="col-md-8 order-md-1 animatorFields">
 				<h4 class="mb-3">Informations -Animateur/trice</h4>
-				<form class="needs-validation" novalidate="">
+				<form class="needs-validation" novalidate="" action="signIn.php" method="POST">
 					<div class="row">
 						<div class="col-md-6 mb-3">
-							<label for="firstName">First name</label>
-							<input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
+							<label for="firstName">Prénom</label>
+							<input type="text" class="form-control" id="firstName" placeholder="Votre prénom" name="firstName" value="" required="">
 							<div class="invalid-feedback">
 								Valid first name is required.
 							</div>
 						</div>
 						<div class="col-md-6 mb-3">
-							<label for="lastName">Last name</label>
-							<input type="text" class="form-control" id="lastName" placeholder="" value="" required="">
+							<label for="lastName">Nom</label>
+							<input type="text" class="form-control" id="lastName" name="lastName" placeholder="Votre nom" value="" required="">
 							<div class="invalid-feedback">
 								Valid last name is required.
 							</div>
@@ -47,130 +47,71 @@
 					</div>
 
 					<div class="mb-3">
-						<label for="username">Username</label>
-						<div class="input-group">
-							<div class="input-group-prepend">
-								<span class="input-group-text">@</span>
-							</div>
-							<input type="text" class="form-control" id="username" placeholder="Username" required="">
-							<div class="invalid-feedback" style="width: 100%;">
-								Your username is required.
-							</div>
-						</div>
-					</div>
-
-					<div class="mb-3">
-						<label for="email">Email <span class="text-muted">(Optional)</span></label>
-						<input type="email" class="form-control" id="email" placeholder="you@example.com">
+						<label for="email">Courriel</label>
+						<input type="email" class="form-control" id="email" name="email" placeholder="vous@exemple.com">
 						<div class="invalid-feedback">
-							Please enter a valid email address for shipping updates.
+							Erreur dans l'adresse courriel.
 						</div>
 					</div>
 
 					<div class="mb-3">
-						<label for="address">Address</label>
-						<input type="text" class="form-control" id="address" placeholder="1234 Main St" required="">
+						<label for="address">Adresse</label>
+						<input type="text" class="form-control" id="address" name="address" placeholder="" required="">
 						<div class="invalid-feedback">
-							Please enter your shipping address.
+							Svp veuillez entrer votre adresse.
 						</div>
 					</div>
 
 					<div class="mb-3">
-						<label for="address2">Address 2 <span class="text-muted">(Optional)</span></label>
-						<input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
+						<label for="address2">Ville</label>
+						<input type="text" class="form-control" id="city" name="city" placeholder="Votre ville">
 					</div>
 
 					<div class="row">
 						<div class="col-md-5 mb-3">
-							<label for="country">Country</label>
-							<select class="custom-select d-block w-100" id="country" required="">
-								<option value="">Choose...</option>
-								<option>United States</option>
+							<label for="country">Pays</label>
+							<select class="custom-select d-block w-100" name="country" id="country" required="">
+								<option value="">Canada</option>
+								<option>États-Unis</option>
 							</select>
 							<div class="invalid-feedback">
 								Please select a valid country.
 							</div>
 						</div>
-						<div class="col-md-4 mb-3">
-							<label for="state">State</label>
-							<select class="custom-select d-block w-100" id="state" required="">
-								<option value="">Choose...</option>
-								<option>California</option>
-							</select>
-							<div class="invalid-feedback">
-								Please provide a valid state.
-							</div>
-						</div>
+						
 						<div class="col-md-3 mb-3">
-							<label for="zip">Zip</label>
-							<input type="text" class="form-control" id="zip" placeholder="" required="">
+							<label for="zip">Code postal</label>
+							<input type="text" class="form-control" id="zip" name="zipCode" placeholder="Code postal" required="">
 							<div class="invalid-feedback">
-								Zip code required.
+								Code postal requis.
 							</div>
 						</div>
 					</div>
-					<hr class="mb-4">
-					<div class="custom-control custom-checkbox">
-						<input type="checkbox" class="custom-control-input" id="same-address">
-						<label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
-					</div>
-					<div class="custom-control custom-checkbox">
-						<input type="checkbox" class="custom-control-input" id="save-info">
-						<label class="custom-control-label" for="save-info">Save this information for next time</label>
-					</div>
+
 					<hr class="mb-4">
 
-					<h4 class="mb-3">Payment</h4>
+					<h4 class="mb-3">Mot de passe</h4>
 
-					<div class="d-block my-3">
-						<div class="custom-control custom-radio">
-							<input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked="" required="">
-							<label class="custom-control-label" for="credit">Credit card</label>
-						</div>
-						<div class="custom-control custom-radio">
-							<input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required="">
-							<label class="custom-control-label" for="debit">Debit card</label>
-						</div>
-						<div class="custom-control custom-radio">
-							<input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required="">
-							<label class="custom-control-label" for="paypal">Paypal</label>
-						</div>
-					</div>
+
 					<div class="row">
 						<div class="col-md-6 mb-3">
-							<label for="cc-name">Name on card</label>
-							<input type="text" class="form-control" id="cc-name" placeholder="" required="">
-							<small class="text-muted">Full name as displayed on card</small>
+							<label for="cc-name">Mot de passe</label>
+							<input type="password" class="form-control" id="password" name="password1" placeholder="Mot de passe" required="">
+							<small class="text-muted">Un bon mot de passe contient de 8 à 10 caractères variés (1-9 + #/() + A-Z + a-z ). </small>
 							<div class="invalid-feedback">
-								Name on card is required
+								Le mot de passe est requis
 							</div>
 						</div>
+
 						<div class="col-md-6 mb-3">
-							<label for="cc-number">Credit card number</label>
-							<input type="text" class="form-control" id="cc-number" placeholder="" required="">
+							<label for="cc-name">Récrire le mot de passe</label>
+							<input type="password" class="form-control" id="re-type_password" name="password2" placeholder="Réécrire le mot de passe" required="">
+							<small class="text-muted">Veuillez réécrire le MDP pour confirmer</small>
 							<div class="invalid-feedback">
-								Credit card number is required
+								Le mot de passe est requis
 							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-3 mb-3">
-							<label for="cc-expiration">Expiration</label>
-							<input type="text" class="form-control" id="cc-expiration" placeholder="" required="">
-							<div class="invalid-feedback">
-								Expiration date required
-							</div>
-						</div>
-						<div class="col-md-3 mb-3">
-							<label for="cc-expiration">CVV</label>
-							<input type="text" class="form-control" id="cc-cvv" placeholder="" required="">
-							<div class="invalid-feedback">
-								Security code required
-							</div>
-						</div>
-					</div>
-					<hr class="mb-4">
-					<button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
+					<button class="btn btn-primary btn-lg btn-block" type="submit">S'inscire</button>
 				</form>
 			</div>
 		</div>
